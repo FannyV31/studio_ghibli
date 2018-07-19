5fedfaa36f12412c1a7fd4b57cfc649b748791fc
YOUR_GITHUB_USERNAMEcd <fevillarosa73@gmail.com>
2018-07-19 14:53:52 -0700

    added dependencies

--- django/djreact/urls.py
+++ django/djreact/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views import generic
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^app2/',
+        generic.TemplateView.as_view(template_name='sample_app2.html')),
+    url(r'^$',
+        generic.TemplateView.as_view(template_name='sample_app.html')),
 ]
